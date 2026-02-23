f475aa761b55279a4d5ea9abb10a40978d984982
Vriti-Bansal <tnsq0963@leeds.ac.uk>
2026-02-23 19:10:39 +0000

    end

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -6,17 +6,21 @@ Student ID:201914016
 
 #include <stdio.h>
 #include <string.h>
-
+/* argc= number of command line arguments
+   argv= array of command line arguments
+   first argument is alwyays the name of the program 
+*/
 int main( int argc, char **argv ) {
 
     // define appropriate data to hold your answer
-    char string[100]; 
-
-    strcpy(string,argv[1]);
-    strcat(string,"-");
-    strcat(string,argv[2]);
-    strcat(string,"-");
-    strcat(string,argv[3]);
+    //initializing the string to an empty string
+    char string[100]=""; 
+    for(int i=1; i<argc; i++){
+        strcat(string,argv[i]);
+        if(i<argc-1){       //if its not the last argument then add "-"
+            strcat(string,"-");
+        }
+    }       
 
     printf("%s\n",string); // use only this single print statement in your submitted code
 
